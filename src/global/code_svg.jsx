d94e5cbd8e2cb600fab539795fd32757a909a6a4
Emmanuel <laryeaaristotle@gmail.com>
2025-01-18 19:56:29 +0000

    icons added and animated

---
+++ src/global/code_svg.jsx
@@ -0,0 +1,7 @@
+export const Codesvg = (props) => {
+    return (
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" height={props.height} viewBox="0 -960 960 960" width={props.width} fill={props.color}><path d="M320-240 80-480l240-240 57 57-184 184 183 183-56 56Zm320 0-57-57 184-184-183-183 56-56 240 240-240 240Z" className="rotate-svg"/></svg>
+        </div>
+    )
+}
--- src/screens/details.jsx
+++ src/screens/details.jsx
@@ -1,5 +1,10 @@
+import { Apisvg } from "../global/api_svg"
 import { Card } from "../global/card"
-import { Globe } from "../global/globe"
+import { Codesvg } from "../global/code_svg"
+import { Devsvg } from "../global/dev_svg"
+import { Globe } from "../global/globe_svg"
+import { Graphicsvg } from "../global/graphic_svg"
+import { Maintainsvg } from "../global/maintain_svg"
 
 export const Details = () => {
     return (
@@ -9,7 +14,7 @@ export const Details = () => {
                 <div className="service-content">
                     {/* website dev */}
                     <Card 
-                        logo = {<Globe color="#7200dd" height="50" width="50"/>}
+                        logo = {<Codesvg color="#7200dd" height="50" width="50"/>}
                         card_title = "Website Development"
                         card_brief = "Offer tailored website solutions for businesses, organizations, and individuals. This include..."
                     />
@@ -23,28 +28,28 @@ export const Details = () => {
 
                     {/* backend services */}
                     <Card 
-                        logo = {<Globe color="#7200dd" height="50" width="50"/>}
+                        logo = {<Devsvg color="#7200dd" height="50" width="50"/>}
                         card_title = "Backend Development"
                         card_brief = "Provide robust backend solutions to support advanced features and functionality. Services can include..."
                     />
 
                     {/* api services */}
                     <Card 
-                        logo = {<Globe color="#7200dd" height="50" width="50"/>}
+                        logo = {<Apisvg color="#7200dd" height="50" width="50"/>}
                         card_title = "API Integration Services"
                         card_brief = "Offer seamless integration of third-party APIs to enhance website or application functionality. This includes..."
                     />
 
                     {/* seo */}
                     <Card 
-                        logo = {<Globe color="#7200dd" height="50" width="50"/>}
+                        logo = {<Maintainsvg color="#7200dd" height="50" width="50"/>}
                         card_title = "Website Maintenance and SEO"
                         card_brief = "Provide ongoing support to keep websites running smoothly while improving visibility and performance. Services include..."
                     />
 
                     {/* graphic design services */}
                     <Card 
-                        logo = {<Globe color="#7200dd" height="50" width="50"/>}
+                        logo = {<Graphicsvg color="#7200dd" height="50" width="50"/>}
                         card_title = "Graphic Design"
                         card_brief = "Provide custom design solutions to enhance the visual appeal and branding of websites and digital platforms. Services include..."
                     />
