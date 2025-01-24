d54ea312873ea65c4eb509b85f9e8f31cbac7fc9
Emmanuel <laryeaaristotle@gmail.com>
2025-01-24 00:43:55 +0000

    running build on test

--- src/global/topnav.jsx
+++ src/global/topnav.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import '../assets/css/header.css'
+import { FaBars } from "react-icons/fa";
 
 export const Topnav = () => {
+    const [isOpen, setIsOpen] = useState(true);
+
+    const togglemenu = () => {
+        setIsOpen(!isOpen);
+    }
+
     return (
         <nav className="nav-bar">
             <div className="left-hold">
@@ -9,12 +16,17 @@ export const Topnav = () => {
                 <a href="#" className="hub">GitHub</a>
             </div>
             <div className="right-hold">
-                <a href="#">Home</a>
-                <a href="#">Services</a>
-                <a href="#">Profile</a>
-                {/* <a href="#">Portfolio</a> */}
-                <a href="#">Contact</a>
-                <a id="hire">Hire Me!</a>
+                <div className={isOpen ? "bar active" : "bar"}>
+                    <a href="#">Home</a>
+                    <a href="#">Services</a>
+                    <a href="#">Profile</a>
+                    {/* <a href="#">Portfolio</a> */}
+                    <a href="#">Contact</a>
+                    <a id="hire">Hire Me!</a>
+                </div>
+                <div className="icon" onClick={togglemenu}>
+                    <FaBars />
+                </div>
             </div>
         </nav>
     )
