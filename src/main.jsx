61a1df16df7c0324668f01255649d59c57ccd1b0
Emmanuel <laryeaaristotle@gmail.com>
2025-01-29 01:25:13 +0000

    every screen css moved to 'responsive.css' file

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './assets/css/colors.css';
 import './assets/css/App.css'
+import './assets/css/responsive.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
